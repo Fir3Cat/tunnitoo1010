23987cd90b88ba89a9518ff3c4b4bc97f1a71fa4
Fir3Cat <raagkristina@gmail.com>
2024-10-10 10:37:47 +0300

    Töö on tehtud

--- praktiline_too_10.10.py
+++ praktiline_too_10.10.py
@@ -1,10 +1,21 @@
 """Matemaatilised tehted"""
-
+import math
 
 # küsime kasutajalt ujuvkomaarvu kujul kolmnurga kaatetid a ja b
 
+
+
 # koosta muutuja a, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
-
+a = float(input("Sisesta arv a:"))
 # koosta muutuja b, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
-
+b = float(input("Sisesta arv b:"))
 # meie ülesandeks on leida hüpoteenus c, kolmnurga ümbermõõt ja pindala NB(vastused peavad olema ümardatud sajandikeni)
+c = math.sqrt((a ** 2) + (b ** 2))
+Umbermoot = a + b + c
+SemiPerimeter = Umbermoot/2
+Pindala = math.sqrt(SemiPerimeter * (SemiPerimeter - a) * (SemiPerimeter - b) * (SemiPerimeter - c))
+
+answer1 = round(c, 2)
+answer2 = round(Umbermoot, 2)
+answer3 = round(Pindala, 2)
+print(f"Hüpotenuus on: {answer1} Ümbermõõt on: {answer2} Pindala on {answer3}")
--- tunnitoo_10_10.py
+++ tunnitoo_10_10.py
@@ -1,4 +1,5 @@
 """Matemaatilised tehted"""
+import math
 
 # tingimuslaused if ja else töötavad ainult andmetüübiga bool(ean)
 
@@ -6,9 +7,10 @@
 # booleani võib igasugu moodi esitada kuid kõige levinum on võrdlemine
 # kui tahame teada, kas 1 + 3 on võrdne nelajaga siis kasutame märki ==, töötab ka tekstiga
 # kuid peam olema tähelepanelik, kuna "Punane" ei ole sama mis "punane", kuid "Punane".lower() on sama mis "punane"
+print("Praegu oleme siin")
 print(1 + 3 == 4)
 print("Punanae == punane:")
-print("Punane" == "punane")
+print("Punane" == "punane".capitalize())
 print("Punane.lower() == punane:")
 print("Punane".lower() == "punane")
 
@@ -23,11 +25,21 @@ print(bool(0))
 # mis annavad võimalust kohe kontrollida, kas midagi kehtib või mitte
 # Kuidas saaks kontrollida et sõna "PUNANE" on kirjutatud suurte tähtedega
 # või et "punane" on kirjutatud väikeste tähtedega
-
+print("Kontrollime kas sõna PUNANE on kirjutatud suurte tähtedega")
+print("PUNANE".isupper())
+print("Kontrollime kas sõna punane on kirjutatud väikeste tähtedega")
+print("punane".islower())
 
 # oletame et tahame teada kas arv on paaris või paaritu, kuidas saaks sellist ülesannet lahendada
 # mõtle, kuidas sina saad teada, et arv on paaris
-# kirjuta kood, mis küsib kasutajalt arvu
+# kirjuta kood, mis küsib kasutajalt arv
+print("----------------------")
+arv = 6
+# int(input("Sisesta täisarv:")))
+if arv % 2 == 0:
+    print("Sinu arv on paarisarv")
+else:
+    print("Sinu arv on paaritu arv")
 
 
 # ülesanne
@@ -36,11 +48,22 @@ print(bool(0))
 # ruutvõrrand lahendatakse kordajate a, b, c abil, ruutvõrrandi lahendivalemi järgi
 
 # koosta muutuja a, lisa võimalus kasutajal sisestada arv, muuda see täisarvuks
-
+a = int(input("Sisesta arv a:"))
 # koosta muutuja b, lisa võimalus kasutajal sisestada arv, muuda see täisarvuks
-
+b = int(input("Sisesta arv b:"))
 # koosta muutuja c, lisa võimalus kasutajal sisestada arv, muuda see täisarvuks
-
+c = int(input("Sisesta arv c"))
 # ruutvõrrandil on tavaliselt 2 lahendit või lahendid puuduvad,
-# kuidas saaks kohe üelda kasutajale, et lahendid puuduvad, kuidas neid leida
+diskriminant = (b ** 2) - (4 * a * c)
+# kuidas saaks kohe üelda kasutajale, et lahendid puuduvad,
+if diskriminant < 0:
+    print("lahendid puuduvad")
+else:
+    lahend1 = (-b + math.sqrt(diskriminant)) / (2 * a)
+    lahend2 = (-b - math.sqrt(diskriminant)) / (2 * a)
+    print(f"Lahendid on: x1 = {lahend1} ja x2 = {lahend2}")
+
+
+# kuidas neid leida
+
 # kuidas võiks nimetada muutujaid
